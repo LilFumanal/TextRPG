14eba53b6af1322b7e71a3cbafc9bb95c4348bf2
LilFumanal <charlie.fumanal@gmail.com>
2023-10-31 11:22:02 +0100

    Typage de Charactère et ajout docstring

--- Character.py
+++ Character.py
@@ -1,14 +1,17 @@
+"""Classe mère de tous les personnages du jeu"""
 from abc import ABC, abstractmethod
 
 class Character(ABC):
   def __init__(self, name, pv, attk_dmg) -> None:
-    self.name = name
-    self.pv = pv
-    self.attk_dmg = attk_dmg
+    self.name: str = name
+    self.pv: int = pv
+    self.attk_dmg: int = attk_dmg
   
   def readDetails(self):
-    print(self.name, "has", self.pv, "and will attack with", self.attk_dmg, "damages")
+    """Récupérer les détails du joueur"""
+    print(self.name, "has", self.pv, "PV and will attack with", self.attk_dmg, "damages.")
   
   @abstractmethod
   def attack(Target):
+    """Méthode abstraite, l'attaque se joue différemment si le monstre ou le joueur est attaquant."""
     pass
