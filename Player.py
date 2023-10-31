f0e47e60f266771b54bf4e850569a06ae9c54e6c
LilFumanal <charlie.fumanal@gmail.com>
2023-10-31 14:00:43 +0100

    Classe Player terminee

--- Player.py
+++ Player.py
@@ -3,8 +3,9 @@ import random
 
 class Player(Character):
   def __init__(self, name, pv_max = 20, pv = 20, attk_dmg = 5, buff_attack = 0, buff_pv = 0, exp = 0, lvl = 1) -> None:
-    super().__init__(name, pv, pv_max, attk_dmg)
-    self.buff_attack: int = buff_attack
+    super().__init__(name, pv, pv_max)
+    self.attk_dmg = attk_dmg
+    self.buff_attack: int  = buff_attack
     self.buff_pv: int = buff_pv
     self.exp: int = exp
     self.lvl: int = lvl
@@ -20,13 +21,23 @@ class Player(Character):
     target.pv = target.pv - total
     print(self.name, "attack!", total, "damages done!")
     
-  def heal(self, target):
-    if target.pv == 0:
-      self.pv = self.pv 
+  def heal(self):
+    if self.pv < self.pv_max:
+      add = self.pv_max / 100 * 20
+      self.pv += add
+      print("Healed: +", add, "pvs")
+    else:
+      print("You're already full-life!")
+    
+  def level_up(self):
+    self.lvl += 1
+    self.buff_pv= self.buff_pv + self.pv_max / 100 * 20
+    self.buff_attack= self.buff_attack + self.attk_dmg / 100 * 20
+    print(self.buff_attack, self.buff_pv)
 
 player1 = Player("Andrew")
 player2 = Player("PIERRE")
-player1.readDetails()
-
-player1.attack(player2)
+player2.readDetails()
+player2.level_up()
+player2.heal()
 player2.readDetails()
