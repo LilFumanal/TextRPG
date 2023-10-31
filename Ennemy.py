955e2283325616c242a65d2c645beccb8c5082d9
LilFumanal <charlie.fumanal@gmail.com>
2023-10-31 14:25:59 +0100

    Correction Ennemy.py

--- Ennemy.py
+++ Ennemy.py
@@ -1,22 +1,22 @@
+"""Classe fille de Character définissant les ennemis"""
 from Character import Character
 from Player import Player
 from random import *
 
 
-"""classe fille de Character déffinissant les ennemies"""
 class Ennemy(Character):
-    def __init__(self, name:str = "gobelins", pv:int = 30, pv_max:int=30, XP:int = 10):
+    def __init__(self, name:str = "Gobelin", pv:int = 30, pv_max:int=30, XP:int = 10):
         super().__init__(name, pv, pv_max)
         self.XP = XP
 
-        """Méthode attaque défiissant l'attaque ennemie"""
-        def attack(Target):
-            dmg = randint(0,(Player.attk_dmg+Player.buff_attk))
-            Target.pv = Target.pv-dmg
-            print(f"{name} has done {dmg} damage")
+    def attack(self, target):
+      """Méthode attaque définissant l'attaque ennemie"""
+      dmg = randint(0,(target.attk_dmg+target.buff_attk)) 
+      target.pv = target.pv-dmg
+      print(f"{self.name} has done {dmg} damage")
             
-        def readDetails(self):
-            print(f"Attack from 0 to {Player.attk_dmg+Player.buff_attk} \n 
-                  PV: {self.pv} / {self.pv_max + self.buff_pv} \n 
-                  Level: {self.lvl} \n 
-                  Experience: {self.exp} \n ")
+    def readDetails(self, target):
+      "Donne les détails de l'ennemis: la range de son attaque, ses PVs, et l'expérience qu'il donnera si vaincu"
+      print(f"Attack from 0 to {target.attk_dmg + target.buff_attk} \n 
+      PV: {self.pv} / {self.pv_max} \n 
+      Points d'experience: {self.XP} \n ")
